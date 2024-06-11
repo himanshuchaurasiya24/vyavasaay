4b8768017cdf7ec6bba32aed5836f73dfd7d5e8c
Himanshu Chaurasiya <himanshuchaurasiya24@gmail.com>
2024-06-11 10:33:08 +0530

    added_page_view_animation

--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -7,11 +7,8 @@
 #include "generated_plugin_registrant.h"
 
 #include <window_size/window_size_plugin.h>
-#include <windows_notification/windows_notification_plugin_c_api.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   WindowSizePluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("WindowSizePlugin"));
-  WindowsNotificationPluginCApiRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("WindowsNotificationPluginCApi"));
 }
